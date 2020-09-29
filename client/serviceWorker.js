9e30973434efaec2ea73cfe76674de92d0abc0a4
Emin <mino@Emins-MacBook-Pro.local>
2020-09-30 00:30:34 +0530

    sw running if browser supports

--- client/serviceWorker.js
+++ client/serviceWorker.js
@@ -1,10 +1,14 @@
-// import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 
-// Meteor.startup(() => {
-//   navigator.serviceWorker
-//     .register('/sw.js')
-//     .then(() => console.info('service worker registered'))
-//     .catch(error => {
-//       console.log('ServiceWorker registration failed: ', error);
-//     });
-// });
+Meteor.startup(() => {
+  const swSupported = 'serviceWorker' in navigator;
+  if (!swSupported) {
+    return;
+  }
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => console.info('service worker registered'))
+    .catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+});
