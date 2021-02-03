f5779912c0c494f944e9e0ba2d60fb3f7cb43d40
Emin Durak <emin.tuta.io>
2021-02-03 23:28:47 +0300

    manuals organised in a nice manner

--- client/RouterComponents/Calendar.jsx
+++ client/RouterComponents/Calendar.jsx
@@ -254,8 +254,8 @@ class Calendar extends React.PureComponent {
 
         <Divider />
 
-        <Row>
-          <h3 style={{ textAlign: 'center' }}>Skogen Manuals</h3>
+        <h3 style={{ textAlign: 'center' }}>Skogen Manuals</h3>
+        <Row justify="center">
           <Col md={8}>
             {isSuperAdmin && (
               <ReactDropzone onDrop={this.handleDropDocument}>
