45d4533bab7d37907a10e9c5b8519cb84daed594
Emin Durak <emin.tuta.io>
2021-02-04 23:02:10 +0300

    refactored publuc activity thumb

--- client/UIComponents/PublicActivityThumb.jsx
+++ client/UIComponents/PublicActivityThumb.jsx
@@ -12,131 +12,98 @@ const dateStyle = {
   lineHeight: 1,
 };
 
-class PublicActivityThumb extends React.Component {
-  getEventTimes = (event) => {
-    if (!event) {
-      return;
-    }
+const commonStyle = {
+  color: '#fff',
+  fontWeight: 300,
+  lineHeight: 1,
+};
 
-    if (event.isMultipleDay || event.isFullDay) {
-      return (
-        moment(event.startDate).format('Do MMM') +
-        ' ' +
-        event.startTime +
-        ' – ' +
-        moment(event.endDate).format('Do MMM') +
-        ' ' +
-        event.endTime
-      );
-    } else if (event.startTime) {
-      return `${event.startTime}–${event.endTime} ${moment(
-        event.startDate
-      ).format('DD MMMM')}`;
-    } else {
-      return '';
-    }
-  };
+const imageStyle = {
+  width: '100%',
+  height: 288,
+  objectFit: 'cover',
+};
 
-  renderDate = (date) => {
-    if (!date) {
-      return;
-    }
+const coverClass = 'thumb-cover';
+const coverContainerClass = 'thumb-cover-container ';
 
-    const isPastEvent = !moment(date.startDate).isAfter(yesterday);
-
-    if (isPastEvent) {
-      dateStyle.color = '#aaa';
-    } else {
-      dateStyle.color = '#fff';
-    }
-
-    return (
-      <div
-        key={date.startDate + date.startTime}
-        style={{ marginRight: 16, marginBottom: 16 }}
-      >
-        <div style={{ ...dateStyle, fontSize: 24 }}>
-          {moment(date.startDate).format('DD')}
-        </div>
-        <div style={{ ...dateStyle, fontSize: 15 }}>
-          {moment(date.startDate).format('MMM').toUpperCase()}
-        </div>
-      </div>
-    );
-  };
-
-  renderDates = () => {
-    const { item } = this.props;
-    const futureDates = item.datesAndTimes.filter((date) =>
-      moment(date.startDate).isAfter(yesterday)
-    );
-    const remaining = futureDates.length - 3;
-
-    return (
-      <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-        {futureDates.slice(0, 3).map((date) => this.renderDate(date))}
-        {remaining > 0 && (
-          <div style={{ ...dateStyle, fontSize: 20, marginBottom: 16 }}>
-            + {remaining}
-          </div>
-        )}
-      </div>
-    );
-  };
-
-  render() {
-    const { item } = this.props;
-
-    const commonStyle = {
-      color: '#fff',
-      fontWeight: 300,
-      lineHeight: 1,
-    };
-
-    const imageStyle = {
-      width: '100%',
-      height: 288,
-      objectFit: 'cover',
-    };
-
-    let clickLink = item.isGroup ? `/group/${item._id}` : `/event/${item._id}`;
-    const coverClass = 'thumb-cover';
-    let coverContainerClass = 'thumb-cover-container ';
-
-    return (
-      <div className={coverContainerClass}>
-        <Link to={clickLink}>
-          <div className={coverClass}>
-            <LazyLoadImage
-              alt={item.title}
-              src={item.imageUrl}
-              style={imageStyle}
-              effect="black-and-white"
-              wrapperClassName="thumb-cover-child-lazy-image"
-            />
-          </div>
-
-          <div style={{ position: 'relative', padding: '24px 16px' }}>
-            {this.renderDates()}
-            <h3
-              style={{
-                ...commonStyle,
-                fontSize: 24,
-                marginBottom: 6,
-                lineHeight: '32px',
-                overflowWrap: 'anywhere',
-              }}
-            >
-              {item.isGroup ? item.title : item.title}
-            </h3>
-            <h4 style={{ ...commonStyle, fontSize: 16, lineHeight: '21px' }}>
-              {item.isGroup ? item.readingMaterial : item.subTitle}
-            </h4>
-          </div>
-        </Link>
-      </div>
-    );
+function ThumbDate({ date }) {
+  if (!date) {
+    return null;
   }
+
+  const isPastEvent = !moment(date.startDate).isAfter(yesterday);
+
+  if (isPastEvent) {
+    dateStyle.color = '#aaa';
+  } else {
+    dateStyle.color = '#fff';
+  }
+
+  return (
+    <div style={{ marginRight: 16, marginBottom: 16 }}>
+      <div style={{ ...dateStyle, fontSize: 24 }}>
+        {moment(date.startDate).format('DD')}
+      </div>
+      <div style={{ ...dateStyle, fontSize: 15 }}>
+        {moment(date.startDate).format('MMM').toUpperCase()}
+      </div>
+    </div>
+  );
+}
+
+function PublicActivityThumb({ item }) {
+  const futureDates = item.datesAndTimes.filter((date) =>
+    moment(date.startDate).isAfter(yesterday)
+  );
+  const remaining = futureDates.length - 3;
+
+  const clickLink = item.isGroup ? `/group/${item._id}` : `/event/${item._id}`;
+
+  return (
+    <div className={coverContainerClass}>
+      <Link to={clickLink}>
+        <div className={coverClass}>
+          <LazyLoadImage
+            alt={item.title}
+            src={item.imageUrl}
+            style={imageStyle}
+            effect="black-and-white"
+            wrapperClassName="thumb-cover-child-lazy-image"
+          />
+        </div>
+
+        <div style={{ position: 'relative', padding: '24px 16px' }}>
+          <div
+            style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}
+          >
+            {futureDates.slice(0, 3).map((date) => (
+              <ThumbDate key={date.startDate + date.startTime} date={date} />
+            ))}
+            {remaining > 0 && (
+              <div style={{ ...dateStyle, fontSize: 20, marginBottom: 16 }}>
+                + {remaining}
+              </div>
+            )}
+          </div>
+          <h3
+            style={{
+              ...commonStyle,
+              fontSize: 24,
+              marginBottom: 6,
+              lineHeight: '32px',
+              overflowWrap: 'anywhere',
+            }}
+          >
+            {item.isGroup ? item.title : item.title}
+          </h3>
+          <h4 style={{ ...commonStyle, fontSize: 16, lineHeight: '21px' }}>
+            {item.isGroup ? item.readingMaterial : item.subTitle}
+          </h4>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default PublicActivityThumb;
