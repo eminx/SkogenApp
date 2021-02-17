3beaf36da3a307064a3c245f899165f19729d4df
Emin Durak <emin.tuta.io>
2021-02-17 23:25:58 +0300

    some retouches on the user page

--- client/RouterComponents/users/User.jsx
+++ client/RouterComponents/users/User.jsx
@@ -26,50 +26,56 @@ function User({ user, userWorks, currentUser, isLoading }) {
     <div>
       <Loader isContainer spinning={!user}>
         <Row gutter={36} style={{ padding: 24 }}>
-          <Col lg={8}>
-            <Space align="center">
-              <Avatar
-                src={user.avatar && user.avatar.src}
-                size={80}
-                // onClick={avatarExists ? () => setAvatarModal(true) : null}
-                // style={{ cursor: avatarExists ? 'pointer' : 'default' }}
-              >
-                {user.username[0].toUpperCase()}
-              </Avatar>
-              <div style={{ paddingLeft: 12 }}>
-                <Title
-                  level={4}
-                  style={{ textTransform: 'lowercase', marginTop: 24 }}
+          <Col lg={8} md={24} sm={24}>
+            <Row justify="center">
+              <Space align="center">
+                <Avatar
+                  src={user.avatar && user.avatar.src}
+                  size={80}
+                  // onClick={avatarExists ? () => setAvatarModal(true) : null}
+                  // style={{ cursor: avatarExists ? 'pointer' : 'default' }}
                 >
-                  {user.username}
-                </Title>
-                <Text strong>{getFullName(user)}</Text>
-              </div>
-            </Space>
+                  {user.username[0].toUpperCase()}
+                </Avatar>
+                <div style={{ paddingLeft: 12 }}>
+                  <Title
+                    level={4}
+                    style={{ textTransform: 'lowercase', marginTop: 24 }}
+                  >
+                    {user.username}
+                  </Title>
+                  <Text strong>{getFullName(user)}</Text>
+                </div>
+              </Space>
+            </Row>
             <Divider />
-            {user.bio && (
-              <Paragraph margin={{ top: 'small' }}>
-                {renderHTML(user.bio)}
-              </Paragraph>
-            )}
+            <Row justify="center">
+              {user.bio && (
+                <Paragraph style={{ maxWidth: 480 }}>
+                  {renderHTML(user.bio)}
+                </Paragraph>
+              )}
+            </Row>
 
             {/* <Link onClick={onOpen} as="button" margin={{ top: 'medium' }}>
               Contact
             </Link> */}
           </Col>
 
-          <Col lg={16}>
-            <Space align="center">
-              {userWorks &&
-                userWorks.length > 0 &&
-                userWorks.map((work) => (
-                  <div key={work._id} style={{ margin: 12 }}>
-                    <Link to={`/${work.authorUsername}/work/${work._id}`}>
-                      <WorkThumb work={work} />
-                    </Link>
-                  </div>
-                ))}
-            </Space>
+          <Col lg={16} md={24} sm={24}>
+            <Row justify="center" style={{}}>
+              <Space align="center">
+                {userWorks &&
+                  userWorks.length > 0 &&
+                  userWorks.map((work) => (
+                    <div key={work._id} style={{ margin: 12 }}>
+                      <Link to={`/${work.authorUsername}/work/${work._id}`}>
+                        <WorkThumb work={work} />
+                      </Link>
+                    </div>
+                  ))}
+              </Space>
+            </Row>
           </Col>
         </Row>
       </Loader>
