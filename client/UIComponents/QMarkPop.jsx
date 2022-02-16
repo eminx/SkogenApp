5c7c4ee6bd04d226ad59afa2f7b26bbd088bb5ad
Emin Durak <emin@tuta.io>
2022-02-17 00:01:35 +0300

    tooltip centered

--- client/UIComponents/QMarkPop.jsx
+++ client/UIComponents/QMarkPop.jsx
@@ -5,7 +5,7 @@ const popoverStyle = { maxWidth: 280, fontSize: 14, lineHeight: 1.3 };
 
 function QMarkPop({ children }) {
   return (
-    <span style={{ marginLeft: 10, marginTop: 6, cursor: 'help' }}>
+    <span style={{ marginLeft: 10, marginTop: 3, cursor: 'help' }}>
       <Popover content={<div style={popoverStyle}>{children}</div>}>
         <img width={24} height={24} src="../../images/QuestionMark.svg" />
       </Popover>
